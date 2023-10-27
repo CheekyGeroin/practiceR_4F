2e05493d48161023df862a39e0a1cb615985359d
Danylo Altukhov <dan.97198@gmail.com>
2023-10-28 00:13:36 +0300

    done task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,62 @@
 import Notiflix from 'notiflix';
-import { Component } from 'react';
+import { useState } from 'react';
 import { Controlers } from './Controlers/Controlers';
 import { Notifications } from './Notifications/Notifications';
 import { Section } from './Section/Section';
 import { Statictics } from './Statistics/Statictics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  onChangeStat = option => {
+  const onChangeStat = option => {
     Notiflix.Notify.success('Thank you for your feedback', { timeout: 1000 });
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
-  };
-  countTotal = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-  countPositivePercentage = () => {
-    return Math.ceil((this.state.good * 100) / this.countTotal());
+    switch (option) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const isThereIsStat = good >= 1 || neutral >= 1 || bad >= 1;
-    const total = this.countTotal();
-    const positivePercentage = this.countPositivePercentage();
-    return (
-      <>
-        <Section title={'Please leave feedback'}>
-          <Controlers
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.onChangeStat}
+  const countTotal = () => {
+    return good + neutral + bad;
+  };
+  const countPositivePercentage = () => {
+    return Math.ceil((good * 100) / countTotal());
+  };
+  const isThereIsStat = good >= 1 || neutral >= 1 || bad >= 1;
+  const total = countTotal();
+  const positivePercentage = countPositivePercentage();
+  return (
+    <>
+      <Section title={'Please leave feedback'}>
+        <Controlers
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onChangeStat}
+        />
+      </Section>
+      <Section title={'Statictics'}>
+        {isThereIsStat ? (
+          <Statictics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-        <Section title={'Statictics'}>
-          {isThereIsStat ? (
-            <Statictics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notifications message={'There is no feedback'} />
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+        ) : (
+          <Notifications message={'There is no feedback'} />
+        )}
+      </Section>
+    </>
+  );
+};
